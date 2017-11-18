e29297010cf75b94f5e28b51d889e981fef2a19d
Grant Jurgensen <gajurgensen@gmail.com>
2017-11-18 13:18:23 -0600

    new dataset to test odd input

---
+++ data/my-data.d
@@ -0,0 +1,14 @@
+< a a a d >
+! This dataset is to test my LERS reader on unusually structured files - such as those with newlines in the middle of a data row
+! When read in, it should be identical to symb.txt
+[ Capacity Noise Comfort Quality ]
+five high
+      low low
+five                             low medium   medium
+five     high medium medium
+
+                                                        !comment
+
+                                                        four low low medium
+        four low  medium       low
+four high medium low
